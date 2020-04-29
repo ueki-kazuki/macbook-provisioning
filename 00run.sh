25908903432879e5c7b8a1cecac6cc6780312ac8
Kazuki Ueki <kazuki@joetsu.ne.jp>
2020-04-30 08:16:21 +0900

    Download japanesque iTerm2 theme

--- 00run.sh
+++ 00run.sh
@@ -7,3 +7,7 @@ brew install mas
 brew bundle
 cp -f $(brew --cellar ricty)/*/share/fonts/Ricty*.ttf ~/Library/Fonts
 fc-cache -vf
+
+if [ ! -f ~/Documents/Japanesque.itermcolors ]; then
+  (cd ~/Documents && curl -sLO https://raw.githubusercontent.com/aereal/dotfiles/master/colors/Japanesque/Japanesque.itermcolors)
+fi
