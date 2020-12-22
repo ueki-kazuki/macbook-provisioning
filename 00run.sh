8810b25a1a55c14cfe88414be59d0fdb232995a5
Kazuki Ueki <ueki.kazuki@classmethod.jp>
2020-12-22 10:40:38 +0900

    Install mas only if it's not installed. Recreate font cache if new font filles are installed.

--- 00run.sh
+++ 00run.sh
@@ -3,10 +3,18 @@
 if !(which brew > /dev/null); then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
-brew install mas
+if !(which mas > /dev/null); then
+    echo "install mas-cli..."
+    brew install mas
+fi
 brew bundle
-cp -f $(brew --cellar ricty)/*/share/fonts/Ricty*.ttf ~/Library/Fonts
-fc-cache -vf
+
+num_newfonts=$(find $(brew --cellar ricty)/*/share/fonts/Ricty*.ttf -mtime -1 -ls 2>&1 | wc -l)
+if [ ${num_newfonts} -gt 0 ]; then
+    echo "update font cache..."
+    cp -f $(brew --cellar ricty)/*/share/fonts/Ricty*.ttf ~/Library/Fonts
+    fc-cache -vf
+fi
 
 if [ ! -f ~/Documents/Japanesque.itermcolors ]; then
   (cd ~/Documents && curl -sLO https://raw.githubusercontent.com/aereal/dotfiles/master/colors/Japanesque/Japanesque.itermcolors)
